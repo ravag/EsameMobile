82365cc8acf66952e53a3405f32e3bd72e309d20
ravag <alerava4@gmail.com>
2026-07-01 17:15:56 +0200

    Modified searchbar implementation

--- app/src/main/java/com/example/esamemobile/screens/HomeScreen.kt
+++ app/src/main/java/com/example/esamemobile/screens/HomeScreen.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -34,6 +36,7 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.messaging.FirebaseMessaging
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.esamemobile.utilities.composables.SimpleSearchBar
 
 @Composable
 fun HomeScreen(
@@ -44,6 +47,7 @@ fun HomeScreen(
     val context = LocalContext.current
     val auth = FirebaseAuth.getInstance()
     var selectedItemIndex by remember { mutableStateOf(0) }
+    val textFieldState = rememberTextFieldState()
 
     val charactersTest = remember {
         listOf(
@@ -66,6 +70,28 @@ fun HomeScreen(
             Group(id = 9, name = "Ci piacciono i treni", ""),
             Group(id = 10, name = "Impottibile!", "")
         )
+    }
+
+    val filteredChars by remember {
+        derivedStateOf {
+            val searchText = textFieldState.text.toString()
+            if (searchText.isEmpty()) {
+                charactersTest
+            } else {
+                charactersTest.filter { it.name.contains(searchText,ignoreCase = true) }
+            }
+        }
+    }
+
+    val filteredGroups by remember {
+        derivedStateOf {
+            val searchText = textFieldState.text.toString()
+            if (searchText.isEmpty()) {
+                groupsTest
+            } else {
+                groupsTest.filter { it.name.contains(searchText,ignoreCase = true) }
+            }
+        }
     }
 
     Scaffold(
@@ -125,25 +151,30 @@ fun HomeScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
+            SimpleSearchBar(
+                textFieldState
             ) {
-                when (selectedItemIndex) {
-                    0 -> {
-                        CharacterList(
-                            contentPadding = innerPadding,
-                            chars = charactersTest,
-                            context = context
-                        )
-                    }
-                    1 -> {
-                        GroupList(
-                            contentPadding = innerPadding,
-                            groups = groupsTest,
-                            context = context
-                        )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    when (selectedItemIndex) {
+                        0 -> {
+                            CharacterList(
+                                contentPadding = innerPadding,
+                                chars = filteredChars,
+                                context = context
+                            )
+                        }
+
+                        1 -> {
+                            GroupList(
+                                contentPadding = innerPadding,
+                                groups = filteredGroups,
+                                context = context
+                            )
+                        }
                     }
                 }
             }
