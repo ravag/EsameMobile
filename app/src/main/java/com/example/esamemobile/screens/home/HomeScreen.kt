44f9dc1c5442b3889195ab1ef8012ad87bcf7b4c
ravag <alerava4@gmail.com>
2026-07-10 17:31:21 +0200

    Removed leftover commented parts of code

--- app/src/main/java/com/example/esamemobile/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/esamemobile/screens/home/HomeScreen.kt
@@ -125,21 +125,6 @@ fun HomeScreen(
                     Text("Debug Database", color = Color.Black, fontSize = 12.sp)
                 }
 
-                //Bottone di logout
-//                Button(
-//                    onClick = {
-//                        FirebaseMessaging.getInstance().unsubscribeFromTopic("tutti")
-//                        Toast.makeText(context, "Logout effettuato", Toast.LENGTH_SHORT).show()
-//                        navController.navigate(EsameMobileRoute.Login)
-//                    },
-//                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
-//                ) {
-//                    Text(
-//                        text = "Logout",
-//                        color = Color.White,
-//                        fontSize = 16.sp
-//                    )
-//                }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
